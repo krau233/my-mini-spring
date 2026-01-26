67d73008f53f3e7c9096267882cb64646519416c
chenyixuan <1406947724@qq.com>
2026-01-26 19:44:01 +0800

    resource-and-resource-loader

---
+++ src/main/java/org/springframework/core/io/ClassPathResource.java
@@ -0,0 +1,27 @@
+package org.springframework.core.io;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class ClassPathResource implements Resource{
+    private final String path;
+
+    public ClassPathResource(String path) {
+        this.path = path;
+    }
+    public String getPath() {
+        return path;
+    }
+
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(this.path);
+        if (is == null) {
+            throw new FileNotFoundException("resource cannot be opened because it does not exist");
+        }
+        return is;
+    }
+}
