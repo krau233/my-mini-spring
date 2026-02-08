694b40ea6396bb7648049ec32bcba1dae324df8f
chenyixuan <1406947724@qq.com>
2026-02-08 19:01:11 +0800

    event-and-event-listener

--- src/test/java/org/springframework/test/ioc/EventAndEventListenerTest.java
+++ src/test/java/org/springframework/test/ioc/EventAndEventListenerTest.java
@@ -2,7 +2,7 @@ package org.springframework.test.ioc;
 
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.ioc.common.events.CustomEvent;
+import org.springframework.test.ioc.common.event.CustomEvent;
 
 /**
  * @author derekyi
--- src/test/java/org/springframework/test/ioc/common/event/ContextClosedEventListener.java
+++ src/test/java/org/springframework/test/ioc/common/event/ContextClosedEventListener.java
@@ -1,4 +1,4 @@
-package org.springframework.test.ioc.common.events;
+package org.springframework.test.ioc.common.event;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
