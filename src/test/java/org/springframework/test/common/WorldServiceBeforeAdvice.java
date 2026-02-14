de90ffff39968d699dc91ca24616daae1243ab8a
chenyixuan <1406947724@qq.com>
2026-02-15 02:55:58 +0800

    pointcut-advisor

--- src/test/java/org/springframework/test/common/WorldServiceBeforeAdvice.java
+++ src/test/java/org/springframework/test/common/WorldServiceBeforeAdvice.java
@@ -1,6 +1,7 @@
 package org.springframework.test.common;
 
 import org.springframework.aop.BeforeAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
 
@@ -8,7 +9,7 @@ import java.lang.reflect.Method;
  * @author derekyi
  * @date 2020/12/6
  */
-public class WorldServiceBeforeAdvice implements BeforeAdvice {
+public class WorldServiceBeforeAdvice implements MethodBeforeAdvice {
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
